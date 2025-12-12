186e3d403f5cb2494a14fa13e59747a9da0e8451
thehealthexpress <thehealthexpress123@gmail.com>
2025-12-12 15:34:12 +0530

    Add order routes for creating and fetching orders

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { createOrder, getOrders } = require('../controllers/orderController');
 
+// Create a new order
+router.post('/', createOrder);
+
+// Get all orders
+router.get('/', getOrders);
+
+module.exports = router;
