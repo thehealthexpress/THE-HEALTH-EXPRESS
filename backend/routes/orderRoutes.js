09c434979fb13bc5dde562cdbb5d186b79699cdf
thehealthexpress <thehealthexpress123@gmail.com>
2025-12-13 10:57:16 +0530

    Update orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { createOrder, getOrders } = require('../controllers/orderController');
+const { createOrder } = require("../controllers/orderController");
 
-// Create a new order
-router.post('/', createOrder);
-
-// Get all orders
-router.get('/', getOrders);
+router.post("/", createOrder);
 
 module.exports = router;
