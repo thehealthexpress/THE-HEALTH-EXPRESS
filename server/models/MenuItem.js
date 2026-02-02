33d14ce0c05f943d1c6f29880bf4ec86c46932cb
thehealthexpress <thehealthexpress123@gmail.com>
2026-02-02 20:04:10 +0530

    Update MenuItem.js

--- server/models/MenuItem.js
+++ server/models/MenuItem.js
@@ -10,3 +10,26 @@ const menuItemSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('MenuItem', menuItemSchema);
+[
+  {
+    "name": "Veggie Salad Bowl",
+    "description": "Fresh mixed veggies with healthy dressing",
+    "price": 12,
+    "category": "Salads",
+    "image": "/images/salad.jpg"
+  },
+  {
+    "name": "Green Smoothie",
+    "description": "Spinach, banana, and almond milk blend",
+    "price": 8,
+    "category": "Drinks",
+    "image": "/images/smoothie.jpg"
+  },
+  {
+    "name": "Quinoa Bowl",
+    "description": "Protein-rich quinoa with veggies",
+    "price": 15,
+    "category": "Main Course",
+    "image": "/images/quinoa.jpg"
+  }
+]
